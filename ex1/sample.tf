1efee3adb4fbc79d91cb59041b2b0ad730aa58e6
2rpadala <rajvja520002@gmail.com>
2023-04-26 10:19:39 +0530

    dns

--- ex1/sample.tf
+++ ex1/sample.tf
@@ -1,8 +1,8 @@
-resource "aws_instance" "web" {
+resource "aws_instance" "testserver" {
   ami           = "ami-0b5a2b5b8f2be4ec2"
   instance_type = "t3.micro"
 
   tags = {
-    Name = "HelloWorld"
+    Name = "test"
   }
 }
