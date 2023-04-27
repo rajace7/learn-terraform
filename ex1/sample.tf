349e1f40361db537b04279df2c4203d2edddc4ae
2rpadala <rajvja520002@gmail.com>
2023-04-27 11:48:44 +0530

    small

--- ex1/sample.tf
+++ ex1/sample.tf
@@ -1,8 +1,21 @@
 resource "aws_instance" "testec2" {
-  ami           = "ami-0b5a2b5b8f2be4ec2"
+  ami           = data.aws_ami.testec2.image_id
   instance_type = "t3.micro"
 
   tags = {
     Name = "testec2"
   }
+}
+
+
+
+  data "aws_ami" "testec2" {
+    most_recent = true
+    owners = ["973714476881"]
+  }
+
+}
+
+output "testec2" {
+  value = data.aws_ami.testec2.image_id
 }
