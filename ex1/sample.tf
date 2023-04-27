11f1964401d4a888221b94fb572b6e887671affc
2rpadala <rajvja520002@gmail.com>
2023-04-27 10:25:19 +0530

    ec2

--- ex1/sample.tf
+++ ex1/sample.tf
@@ -1,8 +1,8 @@
-resource "aws_instance" "testserver" {
+resource "aws_instance" "testec2" {
   ami           = "ami-0b5a2b5b8f2be4ec2"
   instance_type = "t3.micro"
 
   tags = {
-    Name = "test"
+    Name = "testec2"
   }
 }
