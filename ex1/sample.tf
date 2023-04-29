569462643f6933f4cdce131ec08768f38be2221c
2rpadala <rajvja520002@gmail.com>
2023-04-29 10:30:55 +0530

    secuirty group

--- ex1/sample.tf
+++ ex1/sample.tf
@@ -2,11 +2,18 @@ resource "aws_instance" "testec2" {
   ami           = data.aws_ami.testec2.image_id
   instance_type = "t3.micro"
 
+  vpc_id     = [data.aws_security_group.allow_all.id]
+
+
   tags = {
     Name = "testec2"
   }
 }
 
+data "aws_security_group" "allow_all"
+{
+name="allow_all"
+}
 
   data "aws_ami" "testec2" {
     most_recent = true
