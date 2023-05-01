01094b3b0712ef960bdd4f1d54959d239c884ff4
2rpadala <rajvja520002@gmail.com>
2023-05-01 20:56:46 +0530

    instance

--- ex1/sample.tf
+++ ex1/sample.tf
@@ -1,5 +1,5 @@
 
-data "aws_security_group" "allow_all" {
+/*data "aws_security_group" "allow_all" {
   name = "allow_all"
 }
 
@@ -28,4 +28,13 @@ output "testec2" {
 output "allow_all" {
   value = data.aws_security_group.allow_all.id
 }
+*/
 
+resource "aws_instance" "test" {
+  ami           = ami-0b5a2b5b8f2be4ec2
+  instance_type = "t3.micro"
+
+  tags = {
+    Name = "ec2instance_test"
+  }
+}
