06fffdcc91bb98c6f45f7c2e344f19c824e0c35c
2rpadala <rajvja520002@gmail.com>
2023-05-01 21:12:36 +0530

    data

--- ex1/sample.tf
+++ ex1/sample.tf
@@ -38,3 +38,15 @@ resource "aws_instance" "test" {
     Name = "ec2instance_test"
   }
 }
+
+
+data "aws_ami" "test" {
+  most_recent      = true
+  name_regex       = "Centos-8-DevOps-Practice"
+  owners           = ["973714476881"]
+}
+
+output "test" {
+  value = data.aws_ami.test.image_id
+}
+
