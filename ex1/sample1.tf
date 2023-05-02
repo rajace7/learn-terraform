6341659ba8bbedfa68bd9806152c61a6804ddd28
2rpadala <rajvja520002@gmail.com>
2023-05-02 19:00:47 +0530

    data ami

--- ex1/sample1.tf
+++ ex1/sample1.tf
@@ -10,9 +10,17 @@ output "aws_ami" {
   value = data.aws_ami.ec2_1.image_id
 }
 
+variable "instance_type" {
+  default = "t3.micro"
+}
+
+output "instance_type" {
+  value = var.instance_type
+}
+
 resource "aws_instance" "ec2_1" {
   ami           = data.aws_ami.ec2_1.image_id
-  instance_type = "t3.micro"
+  instance_type = var.instance_type
 
   tags = {
     Name = "ec2_1"
