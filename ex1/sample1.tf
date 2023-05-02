e99fae553114bd9ac82f47003e294bf56c93a1a1
2rpadala <rajvja520002@gmail.com>
2023-05-02 18:40:29 +0530

    data ami

--- ex1/sample1.tf
+++ ex1/sample1.tf
@@ -1,3 +1,15 @@
+data "aws_ami" "ec2_1" {
+
+  most_recent      = true
+  name_regex       = "Centos-8-DevOps-Practice"
+  owners           = ["973714476881"]
+
+}
+
+output "aws_ami" {
+  value = data.aws_ami.ec2_1.image_id
+}
+
 resource "aws_instance" "ec2_1" {
   ami           = data.aws_ami.ec2_1.image_id
   instance_type = "t3.micro"
@@ -7,14 +19,5 @@ resource "aws_instance" "ec2_1" {
   }
 }
 
-data "aws_ami" "ec2_1" {
 
-  most_recent      = true
-  name_regex       = " Centos-8-DevOps-Practice"
-  owners           = ["973714476881"]
 
-}
-
-output "aws_ami" {
-  value = data.aws_ami.ec2_1.image_id
-}
