de1abb3525d18c5b0da1dc705edb706d01158fcc
2rpadala <rajvja520002@gmail.com>
2023-05-02 17:33:21 +0530

    var

--- 01-variables/vars1.tf
+++ 01-variables/vars1.tf
@@ -2,6 +2,14 @@ variable "sample1" {
   default = 100
 }
 
+variable "sample2" {
+  default = "print the string"
+}
+
 output "sample1" {
   value = var.sample1
+}
+
+output "sample2" {
+  value = var.sample2
 }
