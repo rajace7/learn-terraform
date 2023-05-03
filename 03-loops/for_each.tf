9196c90ca915a13287618ad04b24a79e8bd774e8
2rpadala <rajvja520002@gmail.com>
2023-05-03 17:16:38 +0530

    for each

--- 03-loops/for_each.tf
+++ 03-loops/for_each.tf
@@ -1,8 +1,19 @@
 variable "friends" {
   default ={
-    name = "raja"
-    name = "gana"
-    name = "rama"
+    raja ={
+      name = "raja"
+      count = 10
+    }
+    gana = {
+      name = "gana"
+      count = 20
+    }
+
+    rama = {
+      name = "rama"
+      count = 30
+    }
+
   }
 }
 
@@ -11,7 +22,7 @@ resource "null_resource" "friends" {
   for_each = var.friends
 
   provisioner "local-exec" {
-    command = "echo ${each.key}"
+    command = "echo ${each.key} - ${each.value}"
 
   }
 
