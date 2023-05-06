1bea65d6511ef3f6d76d61caee239496e21bbb83
2rpadala <rajvja520002@gmail.com>
2023-05-06 21:34:07 +0530

    module

--- 01-variables/practice1.tf
+++ 01-variables/practice1.tf
@@ -17,3 +17,11 @@ output "number" {
 output "string2" {
   value = "value of string = ${var.string}"
 }
+
+variable "list11" {
+  default = [ 100, "hello rajesh", true ]
+}
+
+output "list11" {
+  value = var.list11
+}
