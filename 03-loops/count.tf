a95a307c2ecd7781ff2907358d108d74c1e91192
2rpadala <rajvja520002@gmail.com>
2023-05-07 13:31:46 +0530

    vars

--- 03-loops/count.tf
+++ 03-loops/count.tf
@@ -1,5 +1,9 @@
 resource "null_resource" "fruits" {
   count = length(var.fruits)
+
+  provisioner "local-exec" {
+    command = "name of fruit is ${var.fruits[count.index]}"
+  }
 }
 
 variable "fruits" {
