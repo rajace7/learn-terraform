acf3a89f77eb985b24cde27c1d24254e9ba86fba
2rpadala <rajvja520002@gmail.com>
2023-05-07 12:54:59 +0530

    vars

--- practice1/practice_servers.tf
+++ practice1/practice_servers.tf
@@ -10,7 +10,7 @@ output "ami" {
   value = data.aws_ami.ami.image_id
 }
 
-data "aws_security_group" "selected" {
+data "aws_security_group" "allow_all" {
   name = "allow_all"
 }
 
@@ -19,14 +19,17 @@ resource "aws_instance" "frontend" {
   ami           = data.aws_ami.ami.image_id
   instance_type = "t3.micro"
 
+  vpc_security_group_ids = [data.aws_security_group.allow_all.id]
+
   tags = {
     Name = "frontend"
   }
 }
 
 resource "aws_instance" "mongod" {
-  ami           = "ami-0b5a2b5b8f2be4ec2"
+  ami           = data.aws_ami.ami.image_id
   instance_type = "t3.micro"
+  vpc_security_group_ids = [data.aws_security_group.allow_all.id]
 
   tags = {
     Name = "mongod"
@@ -34,11 +37,16 @@ resource "aws_instance" "mongod" {
 }
 
 resource "aws_instance" "catalogue" {
-  ami           = "ami-0b5a2b5b8f2be4ec2"
+  ami           = data.aws_ami.ami.image_id
   instance_type = "t3.micro"
+  vpc_security_group_ids = [data.aws_security_group.allow_all.id]
 
   tags = {
     Name = "catalogue"
   }
+}
+
+output "allow_all" {
+  value = data.aws_security_group.allow_all.id
 }
 
