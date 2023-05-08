98f064c5148df9e982d962ec17dfd741657adfa0
2rpadala <rajvja520002@gmail.com>
2023-05-08 10:34:48 +0530

    state

--- practice_servers.tf
+++ practice_servers.tf
@@ -8,7 +8,26 @@ resource "aws_instance" "instance" {
   vpc_security_group_ids = [data.aws_security_group.allow_all.id]
 
   tags = {
-    Name = each.key
+    Name = each.value["name"]
+  }
+}
+
+resource "null_resource" "provisioners" {
+  depends_on = [aws_instance.instance , aws_route53_record.dns_records]
+  for_each = var.components
+  connection {
+    type     = "ssh"
+    user     = "centos"
+    password = "DevOps321"
+    host     = aws_instance.instance[each.value["name"]].private_ip
+  }
+
+  provisioner "remote-exec" {
+    inline = [
+      "git clone https://github.com/rajace7/learn-terraform.git",
+      "cd learn-terraform",
+      "sudo bash ${each.value["name"]}.sh"
+    ]
   }
 }
 
