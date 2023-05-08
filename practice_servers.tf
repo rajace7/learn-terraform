7fe55264b82c0a9526e059378804a59214b59c6d
2rpadala <rajvja520002@gmail.com>
2023-05-08 12:03:44 +0530

    module

--- practice_servers.tf
+++ practice_servers.tf
@@ -2,6 +2,6 @@ module "practice_servers" {
   for_each = var.components
   source = "./module"
   components_name = each.value["name"]
-  env = each.env
+  env = var.env
   aws_instance_type = each.value["instance_type"]
 }
