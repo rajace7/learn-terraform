07764683d119c196659e4dd48dedfbbcb4f4abb2
2rpadala <rajvja520002@gmail.com>
2023-05-09 21:21:37 +0530

    count

--- 03-loops/count.tf
+++ 03-loops/count.tf
@@ -4,7 +4,7 @@ resource "null_resource" "friends" {
 
   provisioner "local-exec" {
 
-    command = "echo friend is ${var.friends[count.index]}"
+    command = "echo friend is ${var.friends[count.index].name}"
   }
 }
 
