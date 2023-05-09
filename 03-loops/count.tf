30645e9285b5e28bd4e0ab7c4d132616343d71c0
2rpadala <rajvja520002@gmail.com>
2023-05-09 21:09:57 +0530

    ssm terraform

--- 03-loops/count.tf
+++ 03-loops/count.tf
@@ -1,18 +1,4 @@
 resource "null_resource" "friends" {
 
-  count = length(var.friends)
-
-provisioner "local-exec" {
-  command = "echo friend name is ${var.friends[count.index]}"
-
-}
-
-}
-
-output "friends" {
-  value = length(var.friends)
-}
-
-variable "friends" {
-  default = ["raj", "ram", "gana"]
+  count =3
 }
