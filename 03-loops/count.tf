2f16c6cc444fe820a4234272f1e7595a1f3c7e92
2rpadala <rajvja520002@gmail.com>
2023-05-15 16:32:09 +0530

    pull ansible roboshop

--- 03-loops/count.tf
+++ 03-loops/count.tf
@@ -1,18 +1,1 @@
-resource "null_resource" "friends" {
-
-  count = length(var.friends)
-
-  provisioner "local-exec" {
-
-    command = "echo friend is ${var.friends[count.index].name} - ${var.friends[count.index].salary}"
-  }
-}
-
-variable "friends" {
-  default = [  {name ="rajesh", salary = "0"},
-    {name ="rajesh1", salary = "1000"},
-    {name ="rajesh2", salary = "020034"},
-
-  ]
-}
 
--- ex1/sample.tf
+++ ex1/sample.tf
@@ -1,61 +1,8 @@
-
-/*data "aws_security_group" "allow_all" {
-  name = "allow_all"
-}
-
-data "aws_ami" "testec2" {
-  most_recent = true
-  owners = ["973714476881"]
-}
-
-resource "aws_instance" "testec2" {
-  ami           = data.aws_ami.testec2.image_id
+resource "aws_instance" "frontend-dev" {
+  ami           = ami-0b5a2b5b8f2be4ec2
   instance_type = "t3.micro"
 
-  vpc_security_group_ids = [ data.aws_security_group.allow_all.id ]
-
-
   tags = {
-    Name = "testec2"
+    Name = "frontend-dev"
   }
-}
-
-
-output "testec2" {
-  value = data.aws_ami.testec2.image_id
-}
-
-output "allow_all" {
-  value = data.aws_security_group.allow_all.id
-}
-*/
-
-resource "aws_instance" "test" {
-  ami           = "ami-0b5a2b5b8f2be4ec2"
-  instance_type = var.instance_type
-  vpc_security_group_ids = [ data.aws_security_group.test.id ]
-
-  tags = {
-    Name = "ec2instance_test"
-  }
-}
-
-
-data "aws_ami" "test" {
-  most_recent      = true
-  name_regex       = "Centos-8-DevOps-Practice"
-  owners           = ["973714476881"]
-}
-
-output "test" {
-  value = data.aws_ami.test.image_id
-}
-
-
-data "aws_security_group" "test" {
-  name = "allow_all"
-}
-
-output "vpc_id" {
-  value = data.aws_security_group.test.id
 }
