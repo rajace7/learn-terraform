508bd53f8c9aacc735c634cdfbc48dd8a7458be8
2rpadala <rajvja520002@gmail.com>
2023-05-15 17:00:17 +0530

    pull ansible roboshop

--- ex1/sample.tf
+++ ex1/sample.tf
@@ -1,7 +1,7 @@
 data "aws_ami" "ami" {
-  //executable_users = ["self"]
+
   most_recent = true
-  name_regex  = "  Centos-8-DevOps-Practice"
+  name_regex  = "Centos-8-DevOps-Practice"
   owners      = ["973714476881"]
 }
 
