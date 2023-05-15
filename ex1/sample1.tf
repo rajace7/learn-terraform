bdfb49679a917ecff6fcee4ab64137ccc5c52c63
2rpadala <rajvja520002@gmail.com>
2023-05-15 16:39:50 +0530

    pull ansible roboshop

--- ex1/sample1.tf
+++ ex1/sample1.tf
@@ -1,31 +1,4 @@
-data "aws_ami" "ec2_1" {
 
-  most_recent      = true
-  name_regex       = "Centos-8-DevOps-Practice"
-  owners           = ["973714476881"]
-
-}
-
-output "aws_ami" {
-  value = data.aws_ami.ec2_1.image_id
-}
-
-variable "instance_type" {
-  default = "t3.small"
-}
-
-output "instance_type" {
-  value = var.instance_type
-}
-
-resource "aws_instance" "ec2_1" {
-  ami           = data.aws_ami.ec2_1.image_id
-  instance_type = var.instance_type
-
-  tags = {
-    Name = "ec2_1"
-  }
-}
 
 
 
